c7ec4a5a4818bd3712fda4e1a3f2343013720e9d
Franco M <franco.mujica@sistemas.edu.bo>
2025-09-21 20:02:36 -0400

    Update Servicios.java

---
+++ Servicios.java
@@ -0,0 +1,29 @@
+package service;
+
+import dao.TaskDAO;
+import model.Task;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Servicios {
+    private TareaDB dao = new TaskDAO();
+
+    public TaskService() {
+        dao.createTable();
+    }
+
+    public void crearTarea(String titulo, String estado, LocalDateTime fechaHora, String prioridad,
+                           String categoria, String recordatorio, String zonaHoraria,
+                           String preferenciasNotificacion, String temaVista) {
+        Tarea t = new Task(titulo, estado, fechaHora, prioridad, categoria, recordatorio, zonaHoraria, preferenciasNotificacion, temaVista);
+        dao.insert(t);
+    }
+
+    public List<Tarea> filtrarPorEstado(String estado) {
+        return dao.getByEstado(estado);
+    }
+
+    public void eliminarPorEstado(String estado) {
+        dao.deleteByEstado(estado);
+    }
+}
