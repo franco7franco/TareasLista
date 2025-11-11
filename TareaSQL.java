b766ea1cd2ff3d26150b2ac04d36632e8c4f039e
Franco M <franco.mujica@sistemas.edu.bo>
2025-11-10 20:19:15 -0400

    Delete TareaSQL.java

--- TareaSQL.java
+++
@@ -1,9 +0,0 @@
-package app.dao;
-
-import java.sql.*;
-
-public class TareaSQL {
-    public static Connection connect() throws SQLException {
-        return DriverManager.getConnection("jdbc:sqlite:tasks.db");
-    }
-}
